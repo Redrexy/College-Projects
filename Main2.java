9197152a01ea76647a89739763d29f70fdc0776b
Redrexy <148160192+Redrexy@users.noreply.github.com>
2023-10-16 18:37:39 -0400

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject2;
+
+import finalproject.Student;
+
+/**
+ *
+ * @author PSU Libraries
+ */
+public class Main2 {
+    public static void main(String[] args){
+        Student2 newStu = new Student2();
+    }
+}
